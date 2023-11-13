ecac56d66b26d517c7b935b5565c37c0a8889493
oleniuh <maxim.oleniukh@gmail.com>
2023-11-13 15:34:42 +0000

    NavBar component
    - Account Menu
    - Mobile Menu
    - NavBar Items
    - NavBar changes color when scrolled

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import useCurrentUser from "@/hooks/UseCurrentUser";
+import NavBar from "@/components/NavBar";
 import { NextPageContext } from "next";
-import { getSession, signOut } from "next-auth/react";
+import { getSession } from "next-auth/react";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -19,13 +19,9 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-  const { data: user } = useCurrentUser();
-
   return (
     <>
-      <h1 className="text-2xl text-red-900">Netflix Clone</h1>
-      <p className="text-white">Logged in as: {user?.name}</p>
-      <button className="h-10 w-full bg-white" onClick={() => signOut()}>Logout</button>
+    <NavBar />
     </>
   )
 }
